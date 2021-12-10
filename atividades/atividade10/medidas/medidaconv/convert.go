1cdaa8e6c063c822ab907a5479dc1b7a3a60a0d4
Erick Correia Silva <silvaerick331@gmail.com>
2021-12-10 17:53:45 -0300

    Delete atividades/atividade10/medidas/medidaconv directory

--- atividades/atividade10/medidas/medidaconv/convert.go
+++
@@ -1,7 +0,0 @@
-package medidaconv
-
-//convertendo pés para metro
-func PToM(p Pes) Metro { return Metro(p / 3.2808) }
-
-//convertendo metro para pés
-func MToP(m Metro) Pes { return Pes(m * 3.2808) }
