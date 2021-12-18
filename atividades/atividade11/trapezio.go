00aa95602daa6e2e493c01d0923cc6a65f42602b
Erick Correia Silva <silvaerick331@gmail.com>
2021-12-17 21:02:38 -0300

    resolução da atividade 11

--- atividades/atividade11/trapezio.go
+++ atividades/atividade11/trapezio.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 )
+
+var sum float64 = 0.0
+
+func Integral(valor_1 float64) float64 {
+	//Função a integrar
+	return math.Sqrt(1 + math.Sin(valor_1)*math.Sin(valor_1))
+}
 
 func main() {
 
@@ -16,7 +24,7 @@ func main() {
 	//convertendo os argumentos para float 64
 	Limite_inferior, erro1 := strconv.ParseFloat(var1, 64)
 	Limite_superior, erro2 := strconv.ParseFloat(var2, 64)
-	Intervalos, erro3 := strconv.ParseFloat(var3, 64)
+	Intervalos, erro3 := strconv.ParseInt(var3, 10, 64)
 
 	//Se a conversao ter erro,  imprima o primeiro erro e saia do programa
 	if (erro1 != nil) || (erro2 != nil) || (erro3 != nil) {
@@ -27,5 +35,15 @@ func main() {
 	fmt.Println("Limite inferior: ", Limite_inferior)
 	fmt.Println("Limite superior: ", Limite_superior)
 	fmt.Println("Número de Partiçõe:", Intervalos)
+
+	//realizando o calculo
+	Limite_superior = (5*Limite_inferior)/2 - Limite_superior //manipulação na variavel limite superior
+	// Calculando a dimensao de cada intervalo
+	h := (Limite_superior - Limite_inferior) / float64(Intervalos)
+
+	for i := 0; i < int(Intervalos); i++ {
+		sum = sum + Integral(Limite_inferior) + Integral((Limite_inferior+h))*h/2
+	}
+	fmt.Printf("O resultado da soma e: %g", sum)
 
 }
