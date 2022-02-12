e76c7d0b841509e36f7a90c29099aaa9bb9c97a1
Erick Correia Silva <silvaerick331@gmail.com>
2022-02-12 19:43:30 -0300

    resposta at 16

---
+++ atividades/atividade16/expand.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func expand(s string, f func(string) string) string {
+	if len(s)-1 == 0 {
+		return ""
+	}
+
+	if s[0] == '$' {
+		var tamanho_s int
+		tamanho_s = len(s) - 1
+	}
+}
+
+func joker(s string) string {
+	return "HaHAhAhAhaHA " + s
+}
+
+func main() {
+	var input = os.Args[1]
+	input = expand(input, joker)
+	fmt.Printf(input)
+}
